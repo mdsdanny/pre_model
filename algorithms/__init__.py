ef82a28dad552ae7099ddb8c77579d411ccc0448
Daniela Sánchez <dannuu.rs@gmail.com>
2024-12-25 22:50:53 -0300

    Adding two base alghorithms

---
+++ algorithms/__init__.py
@@ -0,0 +1,3 @@
+from algorithm import Algorithm
+from model_pca import ModelPCA
+from model_k_means import ModelKMeans
---
+++ algorithms/algorithm.py
@@ -0,0 +1,8 @@
+class Algorithm:
+    def __init__(self):
+        pass
+
+    def render(self, mpyplot, x_label, y_label, size=None):
+        return None
+
+
